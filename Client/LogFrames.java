b9690d0d74eaa71adc16f16ac940df398a00211b
EdouardPascal <pascaledouard1@gmail.com>
2023-01-14 16:03:29 -0500

    making clode cleaner

--- Client/LogFrames.java
+++ Client/LogFrames.java
@@ -7,34 +7,34 @@ import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
 
 public class LogFrames extends JFrame {
+
+    //variables for west side of frame
+    JPanel titlePanel;
+
+    //variables for the login sidde
     JPanel welcome;
     JLabel welcome_msg;
     JPanel logpanel;
-    JPanel info;
+    JPanel eastPanel;
     JTextField usernameField;
     JPasswordField passwordField;
 
+    //variables for field to grab information
     JPanel usernamePanel = new JPanel();
     JPanel passwordPanel = new JPanel();
     JLabel usernameLabel = new JLabel("Username");
     JLabel passwordLabel = new JLabel("Password");
 
+
     JPanel westPanel = new JPanel();
 
+    JButton create_account = new JButton("Create a new Account");
+    JButton login = new JButton("Login");
 
     public LogFrames(String message) {
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-
+//West side of the frame
         //setting the west panel and adding image
         ImageIcon login_image = new ImageIcon("icon/stockmarketlarge.png");
-        //login_image.setImage(login_image.getImage().getScaledInstance(250, 250, Image.SCALE_DEFAULT));
-
-        JPanel titlePanel = new JPanel();
-
-        westPanel.setBorder(BorderFactory.createEmptyBorder(0, 0, 100, 100));
-        // westPanel.add(new JLabel(login_image));
-        westPanel.setBackground(Color.WHITE);
         JLabel westLabel = new JLabel("StockMaster");
         westLabel.setFont(UIManager.getFont("h1.font"));
 
@@ -42,62 +42,68 @@ public class LogFrames extends JFrame {
         westLabel.setHorizontalTextPosition(SwingConstants.CENTER);
         westLabel.setVerticalAlignment(SwingConstants.TOP);
         westLabel.setIcon(login_image);
+
+
+        titlePanel = new JPanel();
         titlePanel.setBackground(Color.WHITE);
         titlePanel.setLayout(new BoxLayout(titlePanel, BoxLayout.Y_AXIS));
         titlePanel.add(westLabel);
-
-        //titlePanel.add(new JLabel(login_image));
-
-        //JPanel spacer=new JPanel();
-        //spacer.setBackground(B);
         titlePanel.setBorder(BorderFactory.createEmptyBorder(50, 20, 0, 0));
+
+        //configure the west panel of west frame and adding components
+        westPanel.setBorder(BorderFactory.createEmptyBorder(0, 0, 100, 100));
+        westPanel.setBackground(Color.WHITE);
+
         westPanel.add(titlePanel, BorderLayout.CENTER);
-        /////////
-        ;
+
+
+        ////East side
+        ////////////////////////
+        //////////////////////////
+        //////////////////////////
+        //////////////////////////
+        //configure LoginFrame
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setIconImage((new ImageIcon("icon/stock-market.png")).getImage());
+        this.setTitle("StockMaster");
+
+        //configure the top of login side
         welcome = new JPanel();
         welcome_msg = new JLabel(message);
         welcome_msg.setFont(UIManager.getFont("h00.font"));
         welcome_msg.setForeground(new Color(76, 255, 71));
-        //welcome.setBounds(100, 100, 100, 0);
 
-        logpanel = new JPanel();
-        logpanel.setLayout(new
-
-                BoxLayout(logpanel, BoxLayout.X_AXIS));
-        //logpanel.setBorder(BorderFactory.createEmptyBorder(0, 0, 100, 100));
-
-        info = new JPanel();
-        info.setLayout(new BorderLayout(0, 10));
-        info.setBorder(new EmptyBorder(0, 0, 50, 150));
-
-        //     BoxLayout(info, BoxLayout.Y_AXIS));
-        //////////////////////////////////////////////////////////////////////////
-        //////////////////////////////////////////////////////////////////////////
-        //////////////////////////////////////////////////////////////////////////
-        //////////////////////////////////////////////////////////////////////////
-
-
+//configure field to receive user input
         usernameField = new JTextField("USERNAME");
         usernameField.setOpaque(false);
         usernameField.setBorder(new EmptyBorder(0, 0, 10, 10));
-
         usernameField.setColumns(10);
-        //usernameField.setBounds(300, 0, 165, 25);
-/*
-
-        usernamePanel.add(BorderLayout.WEST, usernameLabel);
-        usernamePanel.add(BorderLayout.CENTER, usernameField);
-        passwordPanel.add(BorderLayout.WEST, passwordLabel);
-
-*/
         usernameField.setBorder(BorderFactory.createLineBorder(Color.black));
 
         passwordField = new JPasswordField("PASSWORD");
         passwordField.setColumns(10);
         passwordField.setBorder(BorderFactory.createLineBorder(Color.black));
 
-        //passwordPanel.add(BorderLayout.CENTER, passwordField);
+        //create a panel to put the username field and the password field
+        JPanel userField = new JPanel();
+        userField.setLayout(new BoxLayout(userField, BoxLayout.Y_AXIS));
+        userField.add(usernameField);
+        userField.add(Box.createRigidArea(new Dimension(0, 10)));
+        userField.add(passwordField);
 
+
+        //configure login and create account buttons
+        login.addActionListener(new UserInterface.LoginListener());
+        login.setBackground(new Color(76, 255, 71));
+        login.setForeground(Color.WHITE);
+        login.setMargin(new Insets(30, 30, 30, 25));
+
+
+        create_account.addActionListener(new UserInterface.CreateAccountListener());
+        create_account.setBackground(new Color(76, 255, 71));
+        create_account.setForeground(Color.WHITE);
+        login.setBorder(BorderFactory.createEmptyBorder(0, 0, 30, 150));
+        create_account.setBorder(BorderFactory.createEmptyBorder(0, 30, 30, 150));
 
         usernameField.addFocusListener(new FocusListener() {
             @Override
@@ -132,22 +138,27 @@ public class LogFrames extends JFrame {
         });
 
 
-        //logpanel.setOpaque(false);
-        //////////////////////////////////////////////////////////////////////////////////////////
-        info.add(welcome_msg, BorderLayout.NORTH);
+/////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////
 
-        JPanel userField = new JPanel();
-        userField.setLayout(new BoxLayout(userField, BoxLayout.Y_AXIS));
+        //create a panel for login and create account buttons
+        logpanel = new JPanel();
+        logpanel.setLayout(new BoxLayout(logpanel, BoxLayout.X_AXIS));
+        logpanel.add(login);
+        logpanel.add(Box.createRigidArea(new Dimension(10, 0)));
+        logpanel.add(create_account);
 
-
-        userField.add(usernameField);
-        userField.add(Box.createRigidArea(new Dimension(0, 10)));
-        userField.add(passwordField);
-
-        info.add(userField);
-
-        info.add(logpanel, BorderLayout.SOUTH);
-        // info.setBorder(BorderFactory.createEmptyBorder(westPanel.getWidth(), westPanel.getWidth(), 850, 300));
+        //create panel that will hold everyother panel on the east side of the frame
+        eastPanel = new JPanel();
+        eastPanel.setLayout(new BorderLayout(0, 10));
+        eastPanel.setBorder(new EmptyBorder(0, 0, 50, 150));
+        eastPanel.add(welcome_msg, BorderLayout.NORTH);
+        eastPanel.add(userField);
+        eastPanel.add(logpanel, BorderLayout.SOUTH);
 
 
         this.getContentPane().add(BorderLayout.WEST, westPanel);
--- Client/UserInterface.java
+++ Client/UserInterface.java
@@ -8,7 +8,6 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.sql.*;
 
 
 public class UserInterface {
@@ -32,42 +31,11 @@ public class UserInterface {
 
     //  JTextField usernameField;
     //  JPasswordField passwordField;
-    JButton login = new JButton("Login", JButton.CENTER);
+
 
     public void go() {
         log_page = new LogFrames("Login Page");
-        log_page.setIconImage((new ImageIcon("icon/stock-market.png")).getImage());
-        log_page.setTitle("StockMaster");
-        login.addActionListener(new LoginListener());
-        login.setBackground(new Color(76, 255, 71));
-        login.setForeground(Color.WHITE);
-        //login.setBorder(BorderFactory.createEmptyBorder(0, 0, 30, 150));
 
-
-        // login.setVerticalTextPosition(SwingConstants.CENTER);
-        JButton create_account = new JButton("Create a new Account");
-        create_account.setBackground(new Color(76, 255, 71));
-
-        create_account.setForeground(Color.WHITE);
-        create_account.addActionListener(new CreateAccountListener());
-        login.setBorder(BorderFactory.createEmptyBorder(0, 0, 30, 150));
-        create_account.setBorder(BorderFactory.createEmptyBorder(0, 30, 30, 150));
-
-        //create_account.setPreferredSize((new Dimension(300, 50)));
-
-
-        log_page.logpanel.add(login);
-        log_page.logpanel.add(Box.createRigidArea(new Dimension(10, 0)));
-
-        log_page.logpanel.add(create_account);
-
-
-        //log_page.setSize(300, 300);
-
-
-        // log_page.getContentPane().
-
-        //     add(BorderLayout.EAST, log_page.info);
 
         log_page.setBackground(Color.BLACK);
         log_page.getContentPane().requestFocusInWindow();
@@ -117,53 +85,7 @@ public class UserInterface {
 
     }
 
-    public void connect(String Username, String Password) {
-//Connect to SQL database
-
-        Connection connection = null;
-        try {
-            // db parameters
-            String port = "3306";
-            String directory = "localhost";
-            String database_name = "stockmaster";
-            String url = "jdbc:mysql://" + directory + ":" + port + "/" + database_name;
-            String database_username = "root";
-            String database_password = "stockmaster1";
-            // create a connection to the database
-            connection = DriverManager.getConnection(url, database_username, database_password);
-
-            System.out.println("Connection to SQLite has been established.");
-            /////////look into the table userpass for matching username and password
-            PreparedStatement preparedStatement = (PreparedStatement)
-                    connection.prepareStatement("SELECT Username, Password FROM userpass where Username=? and Password=?");
-            preparedStatement.setString(1, Username);
-            preparedStatement.setString(2, Password);
-            ResultSet resultSet = preparedStatement.executeQuery();
-            if (resultSet.next()) {
-
-                //UserHome ah = new UserHome(userName);
-                //ah.setTitle("Welcome");
-                //ah.setVisible(true);
-                JOptionPane.showMessageDialog(login, "You have successfully logged in");
-            } else {
-                System.out.println("Wrong Username & Password");
-                JOptionPane.showMessageDialog(login, "Wrong Username & Password");
-            }
-
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        } finally {
-            try {
-                if (connection != null) {
-                    connection.close();
-                }
-            } catch (SQLException ex) {
-                System.out.println(ex.getMessage());
-            }
-        }
-    }
-
-
+    
 }
 
 
